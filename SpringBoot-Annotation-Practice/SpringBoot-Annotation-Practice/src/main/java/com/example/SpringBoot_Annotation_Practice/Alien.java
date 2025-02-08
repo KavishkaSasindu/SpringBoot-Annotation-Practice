74f1f4292dede86c33a97e2dc6241aae9697aa03
KavishkaSasindu <sasindu0215@gmail.com>
2025-02-08 15:18:49 +0530

    update the branch setup

--- SpringBoot-Annotation-Practice/SpringBoot-Annotation-Practice/src/main/java/com/example/SpringBoot_Annotation_Practice/Alien.java
+++ SpringBoot-Annotation-Practice/SpringBoot-Annotation-Practice/src/main/java/com/example/SpringBoot_Annotation_Practice/Alien.java
@@ -6,23 +6,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class Alien {
 
-    @Autowired
-    private Computer computer;
 
     public Alien() {
         System.out.println("Alien Object create");
     }
 
     public void code() {
-        computer.compute();
         System.out.println("Coding...");
     }
 
-    public Computer getComputer() {
-        return computer;
-    }
-
-    public void setComputer(Computer computer) {
-        this.computer = computer;
-    }
 }
